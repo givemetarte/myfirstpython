46801e9ca52dd95cd2fc7b7ee0512a37fa6335e0
Haram Park <givemetarte@naver.com>
2019-08-28 22:32:03 +0900

    python day 1
    python day 1: 아주 간단한 것을 훑어보는 수준으로만 공부함...

---
+++ python_day1.py
@@ -0,0 +1,83 @@
+Python 3.7.4 (v3.7.4:e09359112e, Jul  8 2019, 14:54:52) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> input()
+aaaaa
+'aaaaa'
+>>> input('이름을 입력하세요')
+이름을 입력하세요
+''
+>>> input('이름을 입력하세요')
+이름을 입력하세요박하람
+'박하람'
+>>> input('당신의 나이는? :')
+당신의 나이는? :25살
+'25살'
+>>> 
+>>> 
+>>> 
+>>> 
+>>> my_int = 1
+>>> my_int + 4
+5
+>>> my_int * 5
+5
+>>> 박하람 = 3
+>>> 박하람 + 3
+6
+>>> 3.14
+3.14
+>>> 2.14
+2.14
+>>> my_int = 3.14
+>>> my_float = 4.4
+>>> my_int + my_float
+7.540000000000001
+>>> type (my_int)
+<class 'float'>
+>>> type (my_float)
+<class 'float'>
+>>> haram = 3
+>>> type (haram)
+<class 'int'>
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 'hello'
+'hello'
+>>> "hello"
+'hello'
+>>> print ("hello world")
+hello world
+>>> 
+>>> 
+>>> True
+True
+>>> False
+False
+>>> 
+>>> 
+>>> my_bool = True
+>>> print (my_bool)
+True
+>>> print (False)
+False
+>>> students = [김해민, 박현준, 홍성호]
+Traceback (most recent call last):
+  File "<pyshell#39>", line 1, in <module>
+    students = [김해민, 박현준, 홍성호]
+NameError: name '김해민' is not defined
+>>> students = ['김해민', '박현준', '홍성호']
+>>> print ('students')
+students
+>>> print (students)
+['김해민', '박현준', '홍성호']
+>>> 
+>>> 
+>>> 
+>>> 
+>>> print ('hello world') # 안녕?
+hello world
+>>> 
