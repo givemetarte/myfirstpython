975ff32edafec21bb8fec6f34b2f8dd9ac49a14e
Haram Park <givemetarte@naver.com>
2019-08-30 22:54:09 +0900

    Create python_day2.py
    근데 왼손코딩 나랑 안 맞아서 Everybody for Programming: Python 으로 바꿈!  이게 더 좋음

---
+++ python_day2.py
@@ -0,0 +1,32 @@
+Python 3.7.4 (v3.7.4:e09359112e, Jul  8 2019, 14:54:52) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+>>> 
+>>> 
+>>> my_str = "김씨가족"
+>>> 
+>>> 
+>>> print(my_str)
+김씨가족
+>>> 
+>>> 
+>>> type(my_str)
+<class 'str'>
+>>> 
+>>> 
+>>> my_str = '박박하람'
+>>> 
+>>> print (my_str)
+박박하람
+>>> type (my_str)
+<class 'str'>
+>>> 
+>>> 
+>>> my_str = """박하람
+박예람
+김언정"""
+>>> 
+>>> my_str
+'박하람\n박예람\n김언정'
+>>> 
