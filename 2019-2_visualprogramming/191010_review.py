c0cc9956e785bcc12325e4427d7f2cc1ce85fe2b
Haram Park <givemetarte@naver.com>
2019-10-10 21:09:43 +0900

    4주차 수업

--- 2019-2_visualprogramming/191010_review.py
+++ 2019-2_visualprogramming/191010_review.py
@@ -1,4 +1,17 @@
-# how to check 18 down years old
+# credit making process
 
-age = int(input('your age? '))
-print(age)
+score = float(input('what is your mid-term exam score? '))
+
+if score >= 95 :
+    print(' you got A+.')
+elif score >= 90 :
+    print(' you got A0.')
+elif score >= 85 :
+    print('you got B+.')
+elif score >= 80 :
+    print('you got B0.')
+else :
+    print('you got F.')
+
+
+    
---
+++ 2019-2_visualprogramming/191010_visualprogramming.py
@@ -0,0 +1,5 @@
+sentence = input('문장을 입력하세요.')
+print(sentence)
+
+for i in sentence :
+    if 
