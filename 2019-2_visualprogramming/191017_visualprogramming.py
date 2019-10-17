4edc0a4894fe671c578ae2f42e3d239fd4469f29
Haram Park <givemetarte@naver.com>
2019-10-17 19:55:25 +0900

    191017 visual programming

---
+++ 2019-2_visualprogramming/191017_visualprogramming.py
@@ -0,0 +1,4 @@
+def sum_mul(a,b):
+    return a+b, a*b
+
+print(sum_mul(3,4))
---
+++ 2019-2_visualprogramming/191017_visualprogramming2.py
@@ -0,0 +1,114 @@
+Python 3.7.4 (v3.7.4:e09359112e, Jul  8 2019, 14:54:52) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: /Users/harampark/Documents/myfirstpython/2019-2_visualprogramming/191017_visualprogramming.py 
+I eat 3 apples.
+>>> 
+ RESTART: /Users/harampark/Documents/myfirstpython/2019-2_visualprogramming/191017_visualprogramming.py 
+Traceback (most recent call last):
+  File "/Users/harampark/Documents/myfirstpython/2019-2_visualprogramming/191017_visualprogramming.py", line 3, in <module>
+    print('I ate %d apples. I was sick for %s days.' %(number, day))
+NameError: name 'number' is not defined
+>>> 
+ RESTART: /Users/harampark/Documents/myfirstpython/2019-2_visualprogramming/191017_visualprogramming.py 
+I ate 10 apples. I was sick for three days.
+>>> 
+ RESTART: /Users/harampark/Documents/myfirstpython/2019-2_visualprogramming/191017_visualprogramming.py 
+Error code %d%%
+>>> 
+ RESTART: /Users/harampark/Documents/myfirstpython/2019-2_visualprogramming/191017_visualprogramming.py 
+Error code %d%%
+Error code 3%
+>>> 
+ RESTART: /Users/harampark/Documents/myfirstpython/2019-2_visualprogramming/191017_visualprogramming.py 
+        hi
+>>> 
+ RESTART: /Users/harampark/Documents/myfirstpython/2019-2_visualprogramming/191017_visualprogramming.py 
+        hi hello
+>>> 
+ RESTART: /Users/harampark/Documents/myfirstpython/2019-2_visualprogramming/191017_visualprogramming.py 
+3.1416
+>>> 
+ RESTART: /Users/harampark/Documents/myfirstpython/2019-2_visualprogramming/191017_visualprogramming.py 
+Traceback (most recent call last):
+  File "/Users/harampark/Documents/myfirstpython/2019-2_visualprogramming/191017_visualprogramming.py", line 4, in <module>
+    print('%8.4d' %(a,b))
+TypeError: not all arguments converted during string formatting
+>>> 
+ RESTART: /Users/harampark/Documents/myfirstpython/2019-2_visualprogramming/191017_visualprogramming.py 
+put number?(1-10000) :34
++534
+=+534
++534
++534
+=+568
++534
++534
++534
+=+5102
++534
++534
++534
++534
+=+5136
++534
++534
++534
++534
++534
+=+5170
++534
++534
++534
++534
++534
++534
+=+5204
++534
++534
++534
++534
++534
++534
++534
+=+5238
++534
++534
++534
++534
++534
++534
++534
++534
+=+5272
++534
++534
++534
++534
++534
++534
++534
++534
++534
+=+5306
++534
++534
++534
++534
++534
++534
++534
++534
++534
++534
+=+5340
++534
++534
++534
++534
++534
++534
++534
++534
++534
