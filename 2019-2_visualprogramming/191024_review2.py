6a22e79481ad471d9810a94fba9934c51d47b6c7
Haram Park <givemetarte@naver.com>
2019-10-24 12:47:43 +0900

    191024 review2

---
+++ 2019-2_visualprogramming/191024_review2.py
@@ -0,0 +1,358 @@
+Python 3.7.4 (v3.7.4:e09359112e, Jul  8 2019, 14:54:52) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: /Users/harampark/Documents/myfirstpython/2019-2_visualprogramming/191024_review.py 
+2322
+4644
+6966
+9288
+11610
+13932
+16254
+18576
+20898
+23220
+25542
+27864
+30186
+32508
+34830
+37152
+39474
+41796
+44118
+46440
+48762
+51084
+53406
+55728
+58050
+60372
+62694
+65016
+67338
+69660
+71982
+74304
+76626
+78948
+81270
+83592
+85914
+88236
+90558
+92880
+95202
+97524
+99846
+>>> 
+ RESTART: /Users/harampark/Documents/myfirstpython/2019-2_visualprogramming/191024_review.py 
+2322,4644,6966,9288,11610,13932,16254,18576,20898,23220,25542,27864,30186,32508,34830,37152,39474,41796,44118,46440,48762,51084,53406,55728,58050,60372,62694,65016,67338,69660,71982,74304,76626,78948,81270,83592,85914,88236,90558,92880,95202,97524,99846,
+>>> 
+ RESTART: /Users/harampark/Documents/myfirstpython/2019-2_visualprogramming/191024_review.py 
+
+>>> 
+ RESTART: /Users/harampark/Documents/myfirstpython/2019-2_visualprogramming/191024_review.py 
+Traceback (most recent call last):
+  File "/Users/harampark/Documents/myfirstpython/2019-2_visualprogramming/191024_review.py", line 9, in <module>
+    num3.sort()
+AttributeError: 'set' object has no attribute 'sort'
+>>> 
+ RESTART: /Users/harampark/Documents/myfirstpython/2019-2_visualprogramming/191024_review.py 
+Traceback (most recent call last):
+  File "/Users/harampark/Documents/myfirstpython/2019-2_visualprogramming/191024_review.py", line 10, in <module>
+    num3.sort()
+AttributeError: 'set' object has no attribute 'sort'
+>>> 
+ RESTART: /Users/harampark/Documents/myfirstpython/2019-2_visualprogramming/191024_review.py 
+{1, 99847, 83593, 67339, 51085, 34831, 18577, 2323, 85915, 69661, 53407, 37153, 20899, 4645, 88237, 71983, 55729, 39475, 23221, 6967, 90559, 74305, 58051, 41797, 25543, 9289, 92881, 76627, 60373, 44119, 27865, 11611, 95203, 78949, 62695, 46441, 30187, 13933, 97525, 81271, 65017, 48763, 32509, 16255}
+>>> 
+ RESTART: /Users/harampark/Documents/myfirstpython/2019-2_visualprogramming/191024_review.py 
+None
+>>> 
+ RESTART: /Users/harampark/Documents/myfirstpython/2019-2_visualprogramming/191024_review.py 
+[1, 2323, 4645, 6967, 9289, 11611, 13933, 16255, 18577, 20899, 23221, 25543, 27865, 30187, 32509, 34831, 37153, 39475, 41797, 44119, 46441, 48763, 51085, 53407, 55729, 58051, 60373, 62695, 65017, 67339, 69661, 71983, 74305, 76627, 78949, 81271, 83593, 85915, 88237, 90559, 92881, 95203, 97525, 99847]
+>>> 
+ RESTART: /Users/harampark/Documents/myfirstpython/2019-2_visualprogramming/191024_review.py 
+
+[1, 2323, 4645, 6967, 9289, 11611, 13933, 16255, 18577, 20899, 23221, 25543, 27865, 30187, 32509, 34831, 37153, 39475, 41797, 44119, 46441, 48763, 51085, 53407, 55729, 58051, 60373, 62695, 65017, 67339, 69661, 71983, 74305, 76627, 78949, 81271, 83593, 85915, 88237, 90559, 92881, 95203, 97525, 99847]
+>>> 
+ RESTART: /Users/harampark/Documents/myfirstpython/2019-2_visualprogramming/191024_review.py 
+name? 박하람
+address: 01041731781 
+tel:      서울시 동작
+name? 
+ RESTART: /Users/harampark/Documents/myfirstpython/2019-2_visualprogramming/191024_review.py 
+name? 박하람
+address: 01041731781 
+tel:      서울시 동작
+name? 
+ RESTART: /Users/harampark/Documents/myfirstpython/2019-2_visualprogramming/191024_review.py 
+name? 김언정
+address: 01045408387 
+     tel:     서울시 서대문
+name? 
+ RESTART: /Users/harampark/Documents/myfirstpython/2019-2_visualprogramming/191024_review.py 
+name? 김언정
+address: 01045408387 
+ tel:     서울시 서대문
+name? 박근엽
+새로운 데이터입니다.
+새로운 전화번호를 입력하세요. 01045868387
+새로운 주소를 입력하세요. 울산시 중구
+{'김언정': '01045408387', '박하람': '01041731781', '박예람': '01050197780', '박근엽': '01045868387'} {'김언정': '서울시 서대문', '박하람': '서울시 동작', '박예람': '독일 뮌헨', '박근엽': '울산시 중구'}
+name? 박근엽
+address: 01045868387 
+ tel:      울산시 중구
+name? 
+Traceback (most recent call last):
+  File "/Users/harampark/Documents/myfirstpython/2019-2_visualprogramming/191024_review.py", line 17, in <module>
+    name = input('name? ')
+KeyboardInterrupt
+>>> 
+ RESTART: /Users/harampark/Documents/myfirstpython/2019-2_visualprogramming/191024_review.py 
+name? 박예람
+address: 01050197780 
+ tel:       독일 뮌헨
+name? 박예빈
+새로운 데이터입니다.
+새로운 전화번호를 입력하세요. 0104862037
+Traceback (most recent call last):
+  File "/Users/harampark/Documents/myfirstpython/2019-2_visualprogramming/191024_review.py", line 11, in <module>
+    teldic[name][0] = newtel
+KeyError: '박예빈'
+>>> 
+ RESTART: /Users/harampark/Documents/myfirstpython/2019-2_visualprogramming/191024_review.py 
+name? 박예빈
+새로운 데이터입니다.
+새로운 전화번호를 입력하세요. 01023564837
+새로운 주소를 입력하세요. 서울시 동작구
+Traceback (most recent call last):
+  File "/Users/harampark/Documents/myfirstpython/2019-2_visualprogramming/191024_review.py", line 13, in <module>
+    teldic[name][1] = newadd
+IndexError: list assignment index out of range
+>>> 
+ RESTART: /Users/harampark/Documents/myfirstpython/2019-2_visualprogramming/191024_review.py 
+name? 박예빈 
+새로운 데이터입니다.
+새로운 전화번호를 입력하세요. 0102923756
+새로운 주소를 입력하세요. 서울시 동작구
+{'김언정': ['01045408387', '서울시 서대문'], '박하람': ['01041731781', '서울시 동작'], '박예람': ['01050197780', '독일 뮌헨'], '박예빈 ': ['0102923756', '서울시 동작구']}
+name? 박예빈 
+address:  0102923756 
+ tel:     서울시 동작구
+name? 
+ RESTART: /Users/harampark/Documents/myfirstpython/2019-2_visualprogramming/191024_review.py 
+생년월일 8자리를 입력하세요. 19721220
+당신은 1972년 12월 20일 생입니다.
+>>> 
+ RESTART: /Users/harampark/Documents/myfirstpython/2019-2_visualprogramming/191024_review.py 
+한문장을 입력하세요. i am a boy you are a girl.
+i
+am
+a
+boy
+you
+are
+a
+girl.
+총 8단어 입니다.
+>>> 
+ RESTART: /Users/harampark/Documents/myfirstpython/2019-2_visualprogramming/191024_review.py 
+숫자를 입력하세요.(1-10000): 333
+Traceback (most recent call last):
+  File "/Users/harampark/Documents/myfirstpython/2019-2_visualprogramming/191024_review.py", line 9, in <module>
+    print('+ %5d' % list[i])
+IndexError: list index out of range
+>>> 
+ RESTART: /Users/harampark/Documents/myfirstpython/2019-2_visualprogramming/191024_review.py 
+숫자를 입력하세요.(1-10000): 2343
++  2343
+=  2343
++  2343
++  2343
+=  7029
++  2343
++  2343
++  2343
+= 14058
++  2343
++  2343
++  2343
++  2343
+= 23430
++  2343
++  2343
++  2343
++  2343
++  2343
+= 35145
++  2343
++  2343
++  2343
++  2343
++  2343
++  2343
+= 49203
++  2343
++  2343
++  2343
++  2343
++  2343
++  2343
++  2343
+= 65604
++  2343
++  2343
++  2343
++  2343
++  2343
++  2343
++  2343
++  2343
+= 84348
++  2343
++  2343
++  2343
++  2343
++  2343
++  2343
++  2343
++  2343
++  2343
+= 105435
++  2343
++  2343
++  2343
++  2343
++  2343
++  2343
++  2343
++  2343
++  2343
++  2343
+= 128865
++  2343
++  2343
++  2343
++  2343
++  2343
++  2343
++  2343
++  2343
++  2343
++  2343
++  2343
+= 154638
++  2343
++  2343
++  2343
++  2343
++  2343
++  2343
++  2343
++  2343
++  2343
++  2343
++  2343
++  2343
+= 182754Traceback (most recent call last):
+  File "/Users/harampark/Documents/myfirstpython/2019-2_visualprogramming/191024_review.py", line 11, in <module>
+    print('= %5d' % sum)
+KeyboardInterrupt
+>>> 
+ RESTART: /Users/harampark/Documents/myfirstpython/2019-2_visualprogramming/191024_review.py 
+숫자를 입력하세요.(1-10000): 224
++   224
+=   224
+숫자를 입력하세요.(1-10000): 113
++   224
++   113
+=   561
+숫자를 입력하세요.(1-10000): 987
++   224
++   113
++   987
+=  1885
+숫자를 입력하세요.(1-10000): 354
++   224
++   113
++   987
++   354
+=  3563
+숫자를 입력하세요.(1-10000): 252
++   224
++   113
++   987
++   354
++   252
+=  5493
+숫자를 입력하세요.(1-10000): 1000
++   224
++   113
++   987
++   354
++   252
++  1000
+=  8423
+숫자를 입력하세요.(1-10000): 2434
++   224
++   113
++   987
++   354
++   252
++  1000
++  2434
+= 13787
+숫자를 입력하세요.(1-10000): 3535
++   224
++   113
++   987
++   354
++   252
++  1000
++  2434
++  3535
+= 22686
+숫자를 입력하세요.(1-10000): 4968725
++   224
++   113
++   987
++   354
++   252
++  1000
++  2434
++  3535
++ 4968725
+= 5000310
+숫자를 입력하세요.(1-10000): 
+ RESTART: /Users/harampark/Documents/myfirstpython/2019-2_visualprogramming/191024_review.py 
+생년월일을 입력하세요.: 1995,5,3
+Traceback (most recent call last):
+  File "/Users/harampark/Documents/myfirstpython/2019-2_visualprogramming/191024_review.py", line 1, in <module>
+    number = int(input('생년월일을 입력하세요.: '))
+ValueError: invalid literal for int() with base 10: '1995,5,3'
+>>> 
+ RESTART: /Users/harampark/Documents/myfirstpython/2019-2_visualprogramming/191024_review.py 
+생년월일을 입력하세요.: 1995,5,3
+당신의 생일은 1995년 5월 3일입니다.
+>>> 
+ RESTART: /Users/harampark/Documents/myfirstpython/2019-2_visualprogramming/191024_review.py 
+수식을 입력하세요. 더하기만 가능. 34+56+134+2999
+총 34입니다.
+총 90입니다.
+총 224입니다.
+총 3223입니다.
+>>> 
+ RESTART: /Users/harampark/Documents/myfirstpython/2019-2_visualprogramming/191024_review.py 
+수식을 입력하세요. 더하기만 가능. 1234+2525+103958+395726
+503443
+>>> 
+ RESTART: /Users/harampark/Documents/myfirstpython/2019-2_visualprogramming/191024_review.py 
+ 
+>>> 
+ RESTART: /Users/harampark/Documents/myfirstpython/2019-2_visualprogramming/191024_review.py 
+수식을 입력하세요. 더하기만 가능.  34236+83562+38266 
+156064
+>>> 
