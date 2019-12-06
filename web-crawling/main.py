2b470ff48ef6f637c46856feb2f32f20724fe5a1
Haram Park <givemetarte@naver.com>
2019-12-07 00:28:05 +0900

    assignment6

--- web-crawling/main.py
+++ web-crawling/main.py
@@ -3,15 +3,14 @@ from crapper import job_search
 import pandas as pd
 
 
-keyword = input('원하는 직무 분야를 입력하세요.')
+keyword = input('원하는 직무 분야를 입력하세요.: ')
 new_keyword = ''
 for word in keyword:
     if word == ' ':
         word = '+'
     new_keyword += word
 
-HTML = f"https://kr.indeed.com/취업?q={keyword}&l="
-
+HTML = f"https://kr.indeed.com/취업?q={new_keyword}&l="
 
 df = pd.DataFrame(job_search(HTML))
 df.to_csv('joblist.csv')
